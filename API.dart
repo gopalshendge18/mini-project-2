a781b1ce73899b1fb30bcaaf2bbf6192208e7168
gopalshendge18 <85784714+gopalshendge18@users.noreply.github.com>
2022-12-04 13:28:17 +0530

    Update API.dart

--- API.dart
+++ API.dart
@@ -4,7 +4,7 @@ import 'package:nlp/API.dart';
 
 Future<Map> fetchdata(String user, String ans) async {
   http.Response response = await http
-      .get(Uri.parse('https://myapi.herokuapp.com/?user=$user&standard=$ans'));
+      .get(Uri.parse('https://10.40.3.148:8000/?user=$user&standard=$ans'));
 
   Map<String, dynamic> data = jsonDecode(response.body);
   if (response.statusCode == 200) {
